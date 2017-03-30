ce19b106afe36176601d54463783c3ee4cd18dfd
sam <samueljlevin@gmail.com>
2017-03-30 16:17:59 -0400

    reshape after encoder

--- dqn/linnet.lua
+++ dqn/linnet.lua
@@ -9,12 +9,9 @@ require "initenv"
 function create_network(args)
 
     local net = nn.Sequential()
-    net:add(nn.Reshape(unpack(args.input_dims)))
-
-    local nel = 160*180*16
-
-    -- reshape all feature planes into a vector per example
+    local nel = unpack(args.n_units)
     net:add(nn.Reshape(nel))
+
 
     -- fully connected layer
     net:add(nn.Linear(nel, args.n_hid[1]))
--- dqn/linnet_atari3.lua
+++ dqn/linnet_atari3.lua
@@ -4,12 +4,10 @@ Copyright (c) 2014 Google Inc.
 See LICENSE file for full terms of limited license.
 ]]
 
-require 'convnet'
+require 'linnet'
 
 return function(args)
-    args.n_units        = {32, 64, 64}
-    args.filter_size    = {8, 4, 3}
-    args.filter_stride  = {4, 2, 1}
+    args.n_units        = {16*172*152}
     args.n_hid          = {512}
     args.nl             = nn.Rectifier
 

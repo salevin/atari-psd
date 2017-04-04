db9c2e6043536c53a9f4a76436da173ebb749525
Sam <samueljlevin@gmail.com>
2017-04-03 22:14:49 -0400

    changed linnet size

--- dqn/linnet_atari3.lua
+++ dqn/linnet_atari3.lua
@@ -7,7 +7,7 @@ See LICENSE file for full terms of limited license.
 require 'linnet'
 
 return function(args)
-    args.n_units        = {16*172*152}
+    args.n_units        = {16*7*7}
     args.n_hid          = {512}
     args.nl             = nn.Rectifier
 
